f2765b54da747b582added14bff137ffca5d5182
qmli <leeqiming@163.com>
2021-07-17 02:27:13 +0800

    change for micro frond end

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { registerMicroApps, start } from 'qiankun';
+
 createApp(App).mount('#app')
+
+registerMicroApps(
+  [
+    {
+      name: 'react',
+      entry: '//localhost:3002',
+      container: '#subapp-viewport',
+      activeRule: '/react',
+    },
+    {
+      name: 'vue',
+      entry: '//localhost:3001',
+      container: '#subapp-viewport',
+      activeRule: '/vue',
+    },
+  ]
+);
+
+start()
